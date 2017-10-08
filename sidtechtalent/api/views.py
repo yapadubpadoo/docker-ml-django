6dd0d1bbb7641c0cfe03fd918cf57cfe7dba601c
Hiruma <yapadubpadoo@gmail.com>
2017-10-08 14:56:09 +0700

    add movies suggestion api

--- sidtechtalent/api/views.py
+++ sidtechtalent/api/views.py
@@ -5,9 +5,17 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+import json
+from models import MoviesSuggestor
+
 class Hello(APIView):
     def get(self, request):
         return Response({"hello": "world"})
     
 
 # your ml class here
+class MoviesSuggestionView(APIView):
+    def get(self, request):
+        movies_list = MoviesSuggestor().suggest('Star Wars (1977)')
+        return Response(json.dumps(movies_list))
+    
